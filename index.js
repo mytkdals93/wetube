cdf3c94e830b258bbb53c6b4e29ce3d02921774a
LEESANGMIN <mytkdals93@gmail.com>
2020-12-20 09:55:54 +0900

    Add middleware

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
-const express = require("express");
+import express from "express";
+import morgan from "morgan"
+import helmet from "helmet"
+import bodyParser from "body-parser"
+import cookeyParser from "cookie-parser"
 const app = express();
 
 const PORT = 4000;
 
-app.get("/", (req, res) => {
+app.use(helmet());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookeyParser());
+app.use(morgan("dev"));
+
+app.use()
+app.get("/",(req, res) => {
   res.send(`Hello World`);
 });
 app.get("/profile",(req,res)=>{
     res.send(`You are on my profile`)
 })
 
+
 app.listen(PORT, () => {
   console.log(`Listening on: http://localhost:${PORT}`);
 });
+
+//https://nomadcoders.co/wetube/lectures/15
