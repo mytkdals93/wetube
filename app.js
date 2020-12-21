f28988b64376c89c1adaccfa6d731cb51c3858af
LEESANGMIN <mytkdals93@gmail.com>
2020-12-21 11:29:31 +0900

    MVC Pattern

--- app.js
+++ app.js
@@ -1,21 +1,26 @@
 import express from "express";
-import morgan from "morgan"
-import helmet from "helmet"
-import bodyParser from "body-parser"
-import cookeyParser from "cookie-parser"
-import userRouter from "./routers/userRouter"
+import morgan from "morgan";
+import helmet from "helmet";
+import bodyParser from "body-parser";
+import cookeyParser from "cookie-parser";
+import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
+import routes from "./routes";
+import { localsMiddleware } from "./middleware";
 const app = express();
 
 app.use(helmet());
+app.set("view engine", "pug");
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookeyParser());
 app.use(morgan("dev"));
 
-app.use("/",globalRouter);
-app.use("/user",userRouter);
-app.use("/video",videoRouter);
+app.use(localsMiddleware);
+
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter);
+app.use(routes.videos, videoRouter);
 
 export default app;
